ef0f049933bae70204ca6fca7d5ebe2fad646bcb
Berke Bayram <bayramberke@outlook.com.tr>
2024-02-24 15:18:11 +0300

    SVG Files, CSS Update

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 const API_URL = "https://pokeapi.co/api/v2/pokemon/"
-// There are more than 1000 entries in the API 
-const randomID = Math.floor(Math.random() * 1000) + 1
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,20 +13,52 @@ app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
-// app.get("/", async (req, res) => {
-//     try {
-//         const result = await axios.get(API_URL);
-//         console.log(result.data)
-//         res.render("index.ejs", {
-//             content: "test"
-//         });
-//     } 
-//     catch (error) {
-//         res.render("index.ejs", {
-//             content: error.response.data,
-//         })
-//     }
-// });
+app.post("/search", async (req, res) => {
+    try {
+        const name = req.body.pokemonName.toLowerCase();
+        const link = API_URL + name;
+        const result = await axios.get(link);
+        const pokemon = {
+            id: result.data.id,
+            name: result.data.name,
+            height: result.data.height,
+            weight: result.data.weight,
+            abilities: result.data.abilities,
+            cries: result.data.cries.latest,
+            types: result.data.types,
+        }
+        res.render("index.ejs", { pokemon: pokemon });
+    } 
+    catch (error) {
+        res.render("index.ejs", {
+            error: error.response.data,
+        })
+    }
+});
+
+app.post("/random", async (req, res) => {
+    try {
+        // There are more than 1000 entries in the API
+        const randomID = Math.floor(Math.random() * 1000) + 1;
+        const link = API_URL + randomID;
+        const result = await axios.get(link);
+        const pokemon = {
+            id: result.data.id,
+            name: result.data.name,
+            height: result.data.height,
+            weight: result.data.weight,
+            abilities: result.data.abilities,
+            cries: result.data.cries.latest,
+            types: result.data.types,
+        }
+        res.render("index.ejs", { pokemon: pokemon });
+    } 
+    catch (error) {
+        res.render("index.ejs", {
+            error: error.response.data,
+        })
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
